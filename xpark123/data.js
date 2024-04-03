8f3c5436699fd813a241f5dc4682f52976f14b0f
laiw690 <11030340@sssh.tp.edu.tw>
2024-04-03 14:14:30 +0800

    更新資料

---
+++ xpark123/data.js
@@ -0,0 +1,234 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "水母",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1408,
+      "initialViewParameters": {
+        "yaw": -2.486465876383388,
+        "pitch": 0.09308507959052292,
+        "fov": 0.8280972189554987
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0540134894756523,
+          "pitch": 0.28179840147925006,
+          "rotation": 4.71238898038469,
+          "target": "4-"
+        },
+        {
+          "yaw": -1.6314858402190815,
+          "pitch": 0.2891815487986342,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "甘氏巨螯蟹",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.35440869504782313,
+        "pitch": -0.015265566047133916,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5940227879676812,
+          "pitch": 0.18999302996177292,
+          "rotation": 4.71238898038469,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.3417281056252026,
+          "pitch": 0.20220126344398892,
+          "rotation": 1.5707963267948966,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0094588570100438,
+          "pitch": -0.16712516179882542,
+          "title": "甘氏巨螯蟹",
+          "text": "甘氏巨螯蟹（Macrocheira kaempferi）是已知世界上現存體型最大的甲殼動物、十足目短尾下目蜘蛛蟹總科巨螯蟹屬唯一的現存種，因曾有食人的恐怖傳聞，也稱為殺人蟹。"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "魚缸",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.3935895558068907,
+        "pitch": -0.16537696551061742,
+        "fov": 1.087074422015193
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4877282820070281,
+          "pitch": 0.1767188795967769,
+          "rotation": 4.71238898038469,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.2861069069272624,
+          "pitch": 0.17650419221691394,
+          "rotation": 1.5707963267948966,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "樹蛙",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.2279296183918884,
+        "pitch": 0.3451016165674119,
+        "fov": 0.9372189162280329
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7168563277020326,
+          "pitch": 0.4798303819016354,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.152819863760545,
+          "pitch": 0.47173283133605715,
+          "rotation": 1.5707963267948966,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.20730615352442605,
+          "pitch": 0.13570442966979712,
+          "title": "藍箭毒蛙",
+          "text": "藍箭毒蛙（學名：Dendrobates azureus），又名鈷藍箭毒蛙，屬箭毒蛙科（也稱樹棘蛙科），這種蛙目前生活在南蘇利南的河流的兩岸。在巴西和蓋亞那也有一定數量的分布，是第二毒的箭毒蛙（僅次於黃金箭毒蛙，第三名則是三色箭毒蛙）以及世界上第二毒的蛙，世界最毒動物之一。"
+        },
+        {
+          "yaw": -0.4731627028254035,
+          "pitch": 0.5924972460628748,
+          "title": "迷彩箭毒蛙",
+          "text": "迷彩箭毒蛙（學名：Dendrobates auratus）為棲息於中美洲及南美洲西北部的無尾目物種，目前已入侵到美國夏威夷州[1][3][4]。牠們具有十分多樣化的花紋樣式，僅次於染色箭毒蛙及部分草莓箭毒蛙屬物種。<br>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "鰩魚",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
